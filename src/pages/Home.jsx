5cefc790905ccf286bd2cb8f8c32e2e6a91376fc
IrFaRu <irham.f.rufdo@gmail.com>
2022-09-15 08:38:52 +0700

    new challenge

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,17 +17,15 @@ function Home() {
     }, [])
 
     return (
-        <>  
-            <div>
-                {data?.map((item) => {
-                    return (
-                    <div>
-                        <Card image={item.image} username={item.username} followers={item.follower} id={item.id} />
-                    </div>
-                    )
-                })}
-            </div>
-        </>
+        <div>
+            {data?.map((item) => {
+                return (
+                <div>
+                    <Card image={item.image} username={item.username} followers={item.follower} id={item.id} />
+                </div>
+                )
+            })}
+        </div>
     )
 }
 
@@ -35,7 +33,7 @@ function Card(props) {
     return (
             <Link to={`/account/${props.id}`}>
                 <div style={{backgroundColor: "black", height: "100px", display: "flex", margin: "20px 350px"}}>
-                    <img src={props.image} style={{objectFit: "cover", height: "90px", width: "90px", borderRadius: "50%", margin: "5px"}} />
+                    <img src={props.image} alt="img" style={{objectFit: "cover", height: "90px", width: "90px", borderRadius: "50%", margin: "5px"}} />
                     <div sytle={{display: "flex", flexDirection: "column"}}>
                         <h4 style={{color: "white", margin:"20px 0 5px 20px"}}>@{props.username}</h4>
                         <h4 style={{color: "white", margin:"10px 0 10px 20px"}}>Followers : {props.followers}</h4>
--- src/pages/HomeDetail.jsx
+++ src/pages/HomeDetail.jsx
@@ -24,7 +24,7 @@ function HomeDetail() {
         <div style={{backgroundColor: "whitesmoke", margin: "0 350px"}}>
             <div style={{backgroundColor: "grey", height: "100%", width: "100%", display: "flex", flexDirection: "column"}}>
                 <div style={{display: "flex", justifyContent: "center"}}>
-                    <img src={data?.image} style={{ objectFit: "cover", height: "200px", width: "200px", borderRadius: "50%", border: "10px solid magenta"}} />
+                    <img src={data?.image} alt="img" style={{ objectFit: "cover", height: "200px", width: "200px", borderRadius: "50%", border: "10px solid magenta"}} />
                 </div>
                 <div style={{display: "flex", justifyContent: "center"}}>
                     <h2 style={{color: "white" }}>@{data?.username}</h2>
@@ -42,7 +42,7 @@ function HomeDetail() {
             </div>
             <button style={{ alignItems: "center", margin: "50px 100px 150px 300px", padding: "10px" }}>
                 <Link to={'/'} style={{textDecoration: "none"}}>
-                    <img style={{width: "40px"}} src={kisspng} alt="" />
+                    <img style={{width: "40px"}} src={kisspng} alt="img" />
                 </Link>
             </button>
         </div>
