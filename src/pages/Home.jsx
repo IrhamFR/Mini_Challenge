9cb3fd181f58830fdce96151e90a5885dc5ea0c9
IrFaRu <irham.f.rufdo@gmail.com>
2022-09-15 08:15:30 +0700

    new challenge

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
-import { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
-import fakeData from '../fakeData/datafake.json'
+import { useEffect, useState } from "react"
+import { Link } from "react-router-dom"
+// import Container from "react-bootstrap";
+// import { ListGroup, ListGroupItem } from "react-bootstrap";
+import fakeData from "../fakeData/datafake.json"
 
 function Home() {
 
@@ -19,7 +21,7 @@ function Home() {
             <div>
                 {data?.map((item) => {
                     return (
-                    <div>
+                    <div style={{backgroundColor: "white"}}>
                         <Card image={item.image} username={item.username} followers={item.follower} id={item.id} />
                     </div>
                     
@@ -32,17 +34,15 @@ function Home() {
 
 function Card(props) {
     return (
-        <>
             <Link to={`/account/${props.id}`}>
                 <div style={{backgroundColor: "black", height: "100px", display: "flex", margin: "20px 350px"}}>
-                    <img src={props.image} style={{objectFit: "cover", height: "100px", width: "100px", borderRadius: "50%"}} />
+                    <img src={props.image} style={{objectFit: "cover", height: "90px", width: "90px", borderRadius: "50%", margin: "5px"}} />
                     <div sytle={{display: "flex", flexDirection: "column"}}>
-                        <h3 style={{color: "white"}}>@{props.username}</h3>
-                        <h4 style={{color: "white"}}>Followers : {props.followers}</h4>
+                        <h4 style={{color: "white", margin:"20px 0 5px 20px"}}>@{props.username}</h4>
+                        <h4 style={{color: "white", margin:"10px 0 10px 20px"}}>Followers : {props.followers}</h4>
                     </div>
                 </div>
             </Link>
-        </>
     )
 }
 
